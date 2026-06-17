8394df56ccd755dcdfdfb4ac96f23156d8791b30
Nasccped <pedro.main.tec@gmail.com>
2026-06-17 19:29:52 -0300

    refactor: move bool stuff to acrt supertype

---
+++ src/acrt.c
@@ -0,0 +1,103 @@
+#include "acrt.h"
+#include "assertion_counter.h"
+#include "assertion_display.h"
+#include "assertion_wrapper.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Defalt group name for the 'acrt_t' struct. */
+#define DEFAULT_ACRT_NAME "UNAMMED ACRT"
+
+/* If the current assertion shouldn't be ran:
+ * - pointer is null
+ * - previous was fail and 'on_fail' is 'SKIP_ASSERTIONS' */
+int __acrt_should_not_be_ran(acrt_t *);
+
+/* Updates a given acrt pointer based on a result integer. */
+void __acrt_update(acrt_t *, int);
+
+/* Exit the program with a default message if 'on_fail' is set to 'EXIT_PROGRAM'
+ * and previous assertion was failed. */
+void __acrt_fail_exit(acrt_t *);
+
+void __acrt_run_bool_assertion(acrt_t *self, const char *file,
+                               const unsigned int line, const char *expr,
+                               const int result) {
+  if (__acrt_should_not_be_ran(self))
+    return;
+  __acrt_update(self, result);
+  switch (self->display_mode) {
+  case NO_ASSERTION_DISPLAY:
+    break;
+  case DISPLAY_ERRORS_ONLY:
+    if (result) {
+      break;
+    }
+    __attribute__((fallthrough));
+  case DISPLAY_ALL_ASSERTIONS:
+    assertion_wrapper_t w =
+        assertion_wrapper_new_bool(file, line, expr, self->name, result);
+    display_assertion_wrapper(&w);
+    break;
+  }
+  __acrt_fail_exit(self);
+}
+
+acrt_t acrt_new() {
+  return (acrt_t){.name = DEFAULT_ACRT_NAME,
+                  .counter = {0},
+                  .on_fail = EXIT_PROGRAM,
+                  .display_mode = DISPLAY_ERRORS_ONLY,
+                  .previous = NO_PREVIOUS_ASSERTION};
+}
+
+void acrt_set_name(acrt_t *self, const char *name) {
+  if (self && name && strlen(name))
+    self->name = name;
+}
+
+void acrt_reset_counter(acrt_t *self) {
+  if (self)
+    assertion_counter_reset(&self->counter);
+}
+
+void acrt_set_display_variant(acrt_t *self, __acrt_display_t mode) {
+  if (self)
+    self->display_mode = mode;
+}
+
+void acrt_set_on_fail(acrt_t *self, __acrt_on_fail_t on_fail) {
+  if (self)
+    self->on_fail = on_fail;
+}
+
+int __acrt_should_not_be_ran(acrt_t *self) {
+  return (!self) || (self->previous == ASSERTION_WAS_FAILED &&
+                     self->on_fail == SKIP_ASSERTIONS);
+}
+
+void __acrt_update(acrt_t *self, int success) {
+  if (!self)
+    return;
+  assertion_counter_update(&self->counter, success);
+  self->previous = success ? ASSERTION_WAS_PASSED : ASSERTION_WAS_FAILED;
+}
+
+void __acrt_fail_exit(acrt_t *self) {
+  if (!self) {
+    fprintf(stderr, "\n'__acrt_fail_exit' function was called but 'acrt' "
+                    "pointer is null.\n"
+                    "exiting anyway...\n\n");
+    fflush(stderr);
+    exit(1);
+  } else if (self->on_fail == EXIT_PROGRAM &&
+             self->previous == ASSERTION_WAS_FAILED) {
+    fprintf(stderr, "\n'acrt_t->on_fail' was set to 'EXIT_PROGRAM' "
+                    "('acrt_set_on_fail' to change behavior).\n"
+                    "exiting...\n\n");
+    fflush(stderr);
+    exit(1);
+  }
+}
--- src/acrt.h
+++ src/acrt.h
@@ -1,6 +1,75 @@
 #ifndef _ACRT_H_
 #define _ACRT_H_
 
-#include "assert_manager.h"
+#include "assertion_counter.h"
 
-#endif //!_ACRT_H_
+/* Display modes when running an 'acrt_t'. */
+typedef enum {
+  /* Display only on error assertions. */
+  DISPLAY_ERRORS_ONLY,
+  /* Display on all assertions (success included). */
+  DISPLAY_ALL_ASSERTIONS,
+  /* Don't display assertion (even when error). */
+  NO_ASSERTION_DISPLAY,
+} __acrt_display_t;
+
+/* What to do on assertion fails. */
+typedef enum {
+  /* Exit program with error code (1). */
+  EXIT_PROGRAM,
+  /* Continue program and skip all next assertions. */
+  SKIP_ASSERTIONS,
+  /* Continue program and all other assertions normally. */
+  CONTINUE_ASSERTIONS,
+} __acrt_on_fail_t;
+
+/* Holds the previous assertion state (useful when 'SKIP_ASSERTIONS' is on). */
+typedef enum {
+  /* When there's no previous assertion. */
+  NO_PREVIOUS_ASSERTION,
+  /* When the previous assertion was passed. */
+  ASSERTION_WAS_PASSED,
+  /* When the previous assertion was failed. */
+  ASSERTION_WAS_FAILED,
+} __acrt_previous_t;
+
+/* Default asserter manager. */
+typedef struct {
+  /* Name for the current acrt group. */
+  const char *name;
+  /* What to do on fail assertions. */
+  __acrt_on_fail_t on_fail;
+  /* Display mode. */
+  __acrt_display_t display_mode;
+  /* Previous assertion state. */
+  __acrt_previous_t previous;
+  /* Assertion counting. */
+  assertion_counter_t counter;
+} acrt_t;
+
+/* Runs the assertion as boolean expression. */
+void __acrt_run_bool_assertion(acrt_t *, const char *, const unsigned int,
+                               const char *, const int);
+
+/* Runs a boolean assertion. */
+#define acrt_bool(POINTER, EXPR)                                               \
+  __acrt_run_bool_assertion((POINTER), __FILE__, __LINE__, #EXPR, (EXPR))
+
+/* Creates a new acrt struct. The name and counter is set to default. You can
+ * still set a custom name by using 'acrt_set_name' function. */
+acrt_t acrt_new();
+
+/* Set a custom name to an already existing 'acrt' struct. Fails if self/name is
+ * null or name is zero lenght string. */
+void acrt_set_name(acrt_t *, const char *);
+
+/* Reset the counter inner values. */
+void acrt_reset_counter(acrt_t *);
+
+/* Set's a new 'display_variant_t' for the self acrt struct. */
+void acrt_set_display_variant(acrt_t *, __acrt_display_t);
+
+/* Set's a new 'on_fail' action to the self 'acrt'. */
+void acrt_set_on_fail(acrt_t *, __acrt_on_fail_t);
+
+#endif

2775f3d5dc16703ad154f82184fa33895624084f
Nasccped <pedro.main.tec@gmail.com>
2026-06-17 22:18:54 -0300

    fix: remove unnecessary enumerator

--- src/assertion_display.h
+++ src/assertion_display.h
@@ -3,16 +3,6 @@
 
 #include "assertion_wrapper.h"
 
-/* Different modes for assertion displaying. */
-typedef enum {
-  /* Display only error assertions. Keep succeeds silently. */
-  DISPLAY_ERR_ONLY,
-  /* Don't display assertion (even when err). */
-  NO_DISPLAY,
-  /* Display all assertions (even when ok). */
-  DISPLAY_ALL_ASRT,
-} assertion_display_mode_t;
-
 /* Display the expression wrapper. */
 void display_assertion_wrapper(assertion_wrapper_t *);
 

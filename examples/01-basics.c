931703e3f5ce06c1f2e25d31752e4cfe2c1c4996
Nasccped <pedro.main.tec@gmail.com>
2026-06-17 21:35:50 -0300

    fix: change macro call due to latest acrt chage '#4b27299'

--- examples/01-basics.c
+++ examples/01-basics.c
@@ -10,7 +10,7 @@ int main(void) {
   // assert integers
   for (int i = 1; i < 4; i++) {
     printf(" > asserting %d: ", i);
-    acrt_bool(&acrt, i);
+    acrt_bool(acrt, i);
     printf("\x1b[92mok\x1b[0m\n");
   }
 
@@ -18,13 +18,13 @@ int main(void) {
   char *string = "my string";
   printf("\nLet's assert a char pointer\n");
   printf(" > asserting '%s': ", string);
-  acrt_bool(&acrt, string);
+  acrt_bool(acrt, string);
   printf("\x1b[92mok\x1b[0m\n\n");
 
   // fail assert
   int fail = 0;
   printf("Let's do a failable assertion (%d)\n", fail);
-  acrt_bool(&acrt, fail);
+  acrt_bool(acrt, fail);
 
   // the call bellow won't be triggered since the fail panics the program. try
   // switching the 'fail' variable to 1 and see what happens.

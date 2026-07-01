fe7d1fc289d97b1ef215519b601effc210de842c
Nasccped <pedro.main.tec@gmail.com>
2026-07-01 14:36:16 -0300

    refactor: use private func impl for private fields

--- src/acrt.c
+++ src/acrt.c
@@ -1,103 +1,96 @@
 #include "acrt.h"
-#include "assertion_counter.h"
-#include "assertion_display.h"
-#include "assertion_wrapper.h"
 #include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-/* Defalt group name for the 'acrt_t' struct. */
-#define DEFAULT_ACRT_NAME "UNNAMED ACRT"
+/* Default state for the on fail context. */
+static struct __acrt_context_on_fail ON_FAIL_DEFAULT_STATE = {
+    .kind = ON_FAIL_EXIT_PROGRAM_WITH_EXIT_CODE, .action.exit_code = 1};
 
-/* Block that's ran when acrt should exit the program process. */
-#define ACRT_DISPLAY_AND_EXIT()                                                \
-  do {                                                                         \
-    fprintf(stderr, "\n'acrt_t->on_fail' was set to 'EXIT_PROGRAM'.\n"         \
-                    "exiting...\n\n");                                         \
-    fflush(stderr);                                                            \
-    exit(1);                                                                   \
-  } while (0)
+/* Default state for the conting field. */
+static struct __acrt_counting COUNTING_DEFAULT_STATE = {0};
 
-/* If the current assertion should be run:
- * - pointer is not null
- * - previous wasn't fail or 'on_fail' is set to 'CONTINUE_ASSERTIONS' */
-int __acrt_should_run(acrt_t *);
+/* Returns the default __acrt_context_name struct. */
+struct __acrt_context_name build_default_context_name(const char *,
+                                                      const char *);
 
-/* If the current assertion should be displayed:
- * - pointer is not null
- * - display_mode is set to 'DISPLAY_ALL_ASSERTIONS' or ('DISPLAY_ERRORS_ONLY' +
- *   result is err) */
-int __acrt_should_display(acrt_t *, int);
+/* Macro used to avoid repetitive if (!pointer) return; */
+#define IF_TRUE(EXPR, ACTION_BLOCK)                                            \
+  if ((EXPR))                                                                  \
+  ACTION_BLOCK
 
-/* If the current state of acrt pointer requires proccess to be terminated. */
-int __acrt_should_exit(acrt_t *);
-
-/* Updates a given acrt pointer based on a result integer. */
-void __acrt_update(acrt_t *, int);
-
-int __acrt_run_bool_assertion(acrt_t *self, const char *file,
-                              const unsigned int line, const char *expr,
-                              const int result) {
-  if (!__acrt_should_run(self))
-    return result;
-  __acrt_update(self, result);
-  if (__acrt_should_display(self, result)) {
-    assertion_wrapper_t w =
-        assertion_wrapper_new_bool(file, line, expr, self->name, result);
-    display_assertion_wrapper(&w);
-  }
-  if (__acrt_should_exit(self))
-    ACRT_DISPLAY_AND_EXIT();
-  return result;
+acrt_t __acrt_default(const char *file_name, const char *function_name) {
+  struct __acrt_context context = {
+      .name = build_default_context_name(file_name, function_name),
+      .counting = COUNTING_DEFAULT_STATE,
+      .display_mode = DISPLAY_MODE_FAILED_ONLY,
+      .on_fail = ON_FAIL_DEFAULT_STATE};
+  return (acrt_t){.__context = context};
 }
 
-acrt_t acrt_new() {
-  return (acrt_t){.name = DEFAULT_ACRT_NAME,
-                  .counter = {0},
-                  .on_fail = EXIT_PROGRAM,
-                  .display_mode = DISPLAY_ERRORS_ONLY,
-                  .previous = NO_PREVIOUS_ASSERTION};
+void acrt_set_on_fail_exit_program(acrt_t *self, int code) {
+  IF_TRUE(self, {
+    self->__context.on_fail.kind = ON_FAIL_EXIT_PROGRAM_WITH_EXIT_CODE;
+    self->__context.on_fail.action.exit_code = code;
+  })
 }
 
-void acrt_set_name(acrt_t *self, const char *name) {
-  if (self && name && strlen(name))
-    self->name = name;
+void acrt_set_on_fail_ignore_remaining_assertions(acrt_t *self) {
+  IF_TRUE(self, {
+    self->__context.on_fail.kind = ON_FAIL_IGNORE_REMAINING_ASSERTIONS;
+  })
 }
 
-void acrt_reset_counter(acrt_t *self) {
-  if (self)
-    assertion_counter_reset(&self->counter);
+void acrt_set_on_fail_continue_remaining_assertions(acrt_t *self) {
+  IF_TRUE(self, {
+    self->__context.on_fail.kind = ON_FAIL_CONTINUE_REMAINING_ASSERTIONS;
+  })
 }
 
-void acrt_set_display_variant(acrt_t *self, __acrt_display_t mode) {
-  if (self)
-    self->display_mode = mode;
+void acrt_set_on_fail_print_counting_and_exit_program(acrt_t *self, int code) {
+  IF_TRUE(self, {
+    struct __acrt_context_on_fail *fail = &self->__context.on_fail;
+    struct __acrt_counting *counting = &self->__context.counting;
+    fail->kind = ON_FAIL_PRINT_COUNTING_AND_EXIT_PROGRAM_WITH_EXIT_CODE;
+    fail->action.counting_and_exit.counting = counting;
+    fail->action.counting_and_exit.exit_code = code;
+  })
 }
 
-void acrt_set_on_fail(acrt_t *self, __acrt_on_fail_t on_fail) {
-  if (self)
-    self->on_fail = on_fail;
+/* Run function callback and exit the program with the exit code. */
+void acrt_set_on_fail_run_callback_and_exit_program(acrt_t *self,
+                                                    void (*callback)(),
+                                                    int code) {
+  IF_TRUE(self, {
+    self->__context.on_fail.kind = ON_FAIL_RUN_CALLBACK_AND_EXIT_WITH_EXIT_CODE;
+    self->__context.on_fail.action.callback_and_exit.callback_function =
+        callback;
+    self->__context.on_fail.action.callback_and_exit.exit_code = code;
+  })
 }
 
-int __acrt_should_run(acrt_t *self) {
-  return (self) && (self->on_fail == CONTINUE_ASSERTIONS ||
-                    self->previous != ASSERTION_WAS_FAILED);
+void acrt_set_display_mode(acrt_t *self, acrt_display_mode_t mode) {
+  IF_TRUE(self, self->__context.display_mode = mode);
 }
 
-int __acrt_should_display(acrt_t *self, int result) {
-  return (self) && (self->display_mode == DISPLAY_ALL_ASSERTIONS ||
-                    (self->display_mode == DISPLAY_ERRORS_ONLY && !result));
+void acrt_set_context_name_to_file(acrt_t *self) {
+  IF_TRUE(self, self->__context.name.kind = CONTEXT_NAME_USE_FILE_NAME);
 }
 
-int __acrt_should_exit(acrt_t *self) {
-  return (self) && (self->on_fail == EXIT_PROGRAM &&
-                    self->previous == ASSERTION_WAS_FAILED);
+void acrt_set_context_name_to_function(acrt_t *self) {
+  IF_TRUE(self, self->__context.name.kind = CONTEXT_NAME_USE_FUNCTION_NAME);
 }
 
-void __acrt_update(acrt_t *self, int success) {
-  if (!self)
-    return;
-  assertion_counter_update(&self->counter, success);
-  self->previous = success ? ASSERTION_WAS_PASSED : ASSERTION_WAS_FAILED;
+/* Set the acrt pointer context name to a custom char pointer. */
+void acrt_set_context_name_to_custom(acrt_t *self, const char *name) {
+  IF_TRUE(self && name, {
+    self->__context.name.kind = CONTEXT_NAME_USE_CUSTOM_NAME;
+    self->__context.name.name.custom = name;
+  });
+}
+
+/* Returns the default __acrt_context_name struct. */
+struct __acrt_context_name build_default_context_name(const char *file,
+                                                      const char *function) {
+  return (struct __acrt_context_name){
+      .kind = CONTEXT_NAME_USE_FILE_NAME,
+      .name = {.file = file, .function = function, .custom = NULL}};
 }
--- src/acrt.h
+++ src/acrt.h
@@ -1,78 +1,165 @@
 #ifndef _ACRT_H_
 #define _ACRT_H_
 
-#include "assertion_counter.h"
-
-/* Display modes when running an 'acrt_t'. */
+/* Which 'context_name' variant to print? */
 typedef enum {
-  /* Display only on error assertions. */
-  DISPLAY_ERRORS_ONLY,
-  /* Display on all assertions (success included). */
-  DISPLAY_ALL_ASSERTIONS,
-  /* Don't display assertion (even when error). */
-  NO_ASSERTION_DISPLAY,
-} __acrt_display_t;
+  /* Use the file name as name kind (default). */
+  CONTEXT_NAME_USE_FILE_NAME,
 
-/* What to do on assertion fails. */
+  /* Use the function name as name kind. */
+  CONTEXT_NAME_USE_FUNCTION_NAME,
+
+  /* Use a custom naming specified by the user. */
+  CONTEXT_NAME_USE_CUSTOM_NAME
+} acrt_name_kind_t;
+
+/* Naming related stuff to print context at std(out/err). */
+struct __acrt_context_name {
+  /* Name kind for the current context name. */
+  acrt_name_kind_t kind;
+
+  /* Context name inner data. */
+  struct {
+    const char
+        /* Using __FILE__ as context name. */
+        *file,
+
+        /* Using __FUNC__ as context name. */
+        *function,
+
+        /* Using a custom name. */
+        *custom;
+  } name;
+};
+
+/* Action for failed assertions (tagged enum). */
+struct __acrt_context_on_fail {
+  /* Which action to execute. */
+  enum {
+    /* Exit the entire program with the provided exit code at 'action.exit_code'
+     * (default). */
+    ON_FAIL_EXIT_PROGRAM_WITH_EXIT_CODE,
+
+    /* Ignore all remaining assertions. */
+    ON_FAIL_IGNORE_REMAINING_ASSERTIONS,
+
+    /* Continue all remaining assertions. */
+    ON_FAIL_CONTINUE_REMAINING_ASSERTIONS,
+
+    /* Print assertion counting and exit the program with the exit code. */
+    ON_FAIL_PRINT_COUNTING_AND_EXIT_PROGRAM_WITH_EXIT_CODE,
+
+    /* Run a callback function pointer + exit the program with the exit code. */
+    ON_FAIL_RUN_CALLBACK_AND_EXIT_WITH_EXIT_CODE,
+  } kind;
+
+  /* Action inner data. */
+  union {
+    /* Exit code when 'ON_FAIL_EXIT_PROGRAM_WITH_EXIT_CODE' is set. */
+    int exit_code;
+
+    /* Struct when 'ON_FAIL_PRINT_COUNTING_AND_EXIT_PROGRAM_WITH_EXIT_CODE' is
+     * set. */
+    struct {
+      /* The counting pointer. */
+      struct __acrt_counting *counting;
+
+      /* The exit code to return. */
+      int exit_code;
+    } counting_and_exit;
+
+    /* Struct when 'ON_FAIL_RUN_CALLBACK_AND_EXIT_WITH_EXIT_CODE' is set. */
+    struct {
+      /* Callback function. */
+      void (*callback_function)();
+
+      /* Exit code to return. */
+      int exit_code;
+    } callback_and_exit;
+  } action;
+};
+
+/* Assertion counting. */
+struct __acrt_counting {
+  unsigned int
+      /* Total of assertions. */
+      total,
+
+      /* Total of passed assertions. */
+      passed,
+
+      /* Total of failed assertions. */
+      failed,
+
+      /* Total of ignored assertions (when 'ON_FAIL_IGNORE_REMAINING_ASSERTIONS'
+       * is set). */
+      ignored;
+};
+
+/* Display mode for acrt context. */
 typedef enum {
-  /* Exit program with error code (1). */
-  EXIT_PROGRAM,
-  /* Continue program and skip all next assertions. */
-  SKIP_ASSERTIONS,
-  /* Continue program and all other assertions normally. */
-  CONTINUE_ASSERTIONS,
-} __acrt_on_fail_t;
+  /* Display only failed assertions. */
+  DISPLAY_MODE_FAILED_ONLY,
 
-/* Holds the previous assertion state (useful when 'SKIP_ASSERTIONS' is on). */
-typedef enum {
-  /* When there's no previous assertion. */
-  NO_PREVIOUS_ASSERTION,
-  /* When the previous assertion was passed. */
-  ASSERTION_WAS_PASSED,
-  /* When the previous assertion was failed. */
-  ASSERTION_WAS_FAILED,
-} __acrt_previous_t;
+  /* Display both failed and passed assertions. */
+  DISPLAY_MODE_FAILED_AND_PASSED,
 
-/* Default asserter manager. */
+  /* Display all assertions (even when being ignored). */
+  DISPLAY_MODE_ALL,
+
+  /* Don't display any assertion. */
+  DISPLAY_MODE_QUIET
+} acrt_display_mode_t;
+
+/* Acrt private context to handle. */
+struct __acrt_context {
+  struct __acrt_context_name name;
+  struct __acrt_context_on_fail on_fail;
+  struct __acrt_counting counting;
+  acrt_display_mode_t display_mode;
+};
+
+/* Acrt entrypoint. */
 typedef struct {
-  /* Name for the current acrt group. */
-  const char *name;
-  /* What to do on fail assertions. */
-  __acrt_on_fail_t on_fail;
-  /* Display mode. */
-  __acrt_display_t display_mode;
-  /* Previous assertion state. */
-  __acrt_previous_t previous;
-  /* Assertion counting. */
-  assertion_counter_t counter;
+  struct __acrt_context __context;
 } acrt_t;
 
-/* Runs the assertion as boolean expression. Returns an integer at the end
- * meaning the success of assertion. */
-int __acrt_run_bool_assertion(acrt_t *, const char *, const unsigned int,
-                              const char *, const int);
-
-/* Runs a boolean assertion and returns an integer where:
- * - '0' means assertion failed
- * - '1' means assertion passed */
-#define acrt_bool(STRUCT, EXPR)                                                \
-  __acrt_run_bool_assertion(&(STRUCT), __FILE__, __LINE__, #EXPR, (!!(EXPR)))
+/* Create a new 'acrt_t' struct with the default inner values. This function
+ * takes the __FILE__ and __FUNCTION__ strings. */
+acrt_t __acrt_default(const char *, const char *);
 
 /* Creates a new acrt struct. The name and counter is set to default. You can
  * still set a custom name by using 'acrt_set_name' function. */
-acrt_t acrt_new();
+#define acrt_new() __acrt_default(__FILE__, __FUNCTION__)
 
-/* Set a custom name to an already existing 'acrt' struct. Fails if self/name is
- * null or name is zero lenght string. */
-void acrt_set_name(acrt_t *, const char *);
+/* Set inner acrt on fail variant to exit with exit code (second param
+ * (integer)). */
+void acrt_set_on_fail_exit_program(acrt_t *, int);
 
-/* Reset the counter inner values. */
-void acrt_reset_counter(acrt_t *);
+/* Ignore remaining assertions if current one fails. */
+void acrt_set_on_fail_ignore_remaining_assertions(acrt_t *);
 
-/* Set's a new 'display_variant_t' for the self acrt struct. */
-void acrt_set_display_variant(acrt_t *, __acrt_display_t);
+/* Continue remaining assertions if current one fails. */
+void acrt_set_on_fail_continue_remaining_assertions(acrt_t *);
 
-/* Set's a new 'on_fail' action to the self 'acrt'. */
-void acrt_set_on_fail(acrt_t *, __acrt_on_fail_t);
+/* Display current assertion counting and exits the program with the exit
+ * code. */
+void acrt_set_on_fail_print_counting_and_exit_program(acrt_t *, int);
+
+/* Run function callback and exit the program with the exit code. */
+void acrt_set_on_fail_run_callback_and_exit_program(acrt_t *, void (*)(), int);
+
+/* Set a new display mode for the acrt pointer. */
+void acrt_set_display_mode(acrt_t *, acrt_display_mode_t);
+
+/* Set the acrt pointer context name to file (use same name as the file). */
+void acrt_set_context_name_to_file(acrt_t *);
+
+/* Set the acrt pointer context name to function (use same name as the function
+ * scope). */
+void acrt_set_context_name_to_function(acrt_t *);
+
+/* Set the acrt pointer context name to a custom char pointer. */
+void acrt_set_context_name_to_custom(acrt_t *, const char *);
 
 #endif

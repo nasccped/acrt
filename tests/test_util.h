ea9f2c23c31446cc202cb0de2ce0eae2152b7d54
Nasccped <pedro.main.tec@gmail.com>
2026-07-03 15:12:11 -0300

    refactor: macro for string length

--- tests/test_util.h
+++ tests/test_util.h
@@ -11,6 +11,8 @@ static char code_location[128];
 #define SET_CODE_LOCATION                                                      \
   snprintf(code_location, sizeof(code_location), "[%s: %d]", __FILE__, __LINE__)
 
+#define CODE_LOCATION_LENGTH (int)strlen(code_location) + 1
+
 #define DISPLAY_AND_RETURN(FMT, ...)                                           \
   SET_CODE_LOCATION;                                                           \
   fprintf(stderr, "%s " FMT, code_location, __VA_ARGS__);                      \
@@ -19,29 +21,26 @@ static char code_location[128];
 #define ASSERT_EQ_WITH_STRING_FUNC(TITLE, EXPECTED, GOT, STR_FUNC)             \
   if ((EXPECTED) != (GOT)) {                                                   \
     DISPLAY_AND_RETURN("%s assertion failed.\n%*cExpecting %s, got %s\n",      \
-                       (TITLE), (int)strlen(code_location) + 1, ' ',           \
+                       (TITLE), CODE_LOCATION_LENGTH, ' ',                     \
                        STR_FUNC((EXPECTED)), STR_FUNC((GOT)));                 \
   }
 
 #define ASSERT_EQ_POINTER(TITLE, EXPECTED, GOT)                                \
   if ((EXPECTED) != (GOT)) {                                                   \
     DISPLAY_AND_RETURN("%s assertion failed.\n%*cExpecting %p, got %p\n",      \
-                       (TITLE), (int)strlen(code_location) + 1, ' ',           \
-                       (EXPECTED), (GOT));                                     \
+                       (TITLE), CODE_LOCATION_LENGTH, ' ', (EXPECTED), (GOT)); \
   }
 
 #define ASSERT_EQ_INT(TITLE, EXPECTED, GOT)                                    \
   if ((EXPECTED) != (GOT)) {                                                   \
     DISPLAY_AND_RETURN("%s assertion failed.\n%*cExpecting %d, got %d\n",      \
-                       (TITLE), (int)strlen(code_location) + 1, ' ',           \
-                       (EXPECTED), (GOT));                                     \
+                       (TITLE), CODE_LOCATION_LENGTH, ' ', (EXPECTED), (GOT)); \
   }
 
 #define ASSERT_EQ_STRING(TITLE, EXPECTED, GOT)                                 \
   if (strcmp((EXPECTED), (GOT)) != 0) {                                        \
     DISPLAY_AND_RETURN("%s assertion failed.\n%*cExpecting %s, got %s\n",      \
-                       (TITLE), (int)strlen(code_location) + 1, ' ',           \
-                       (EXPECTED), (GOT));                                     \
+                       (TITLE), CODE_LOCATION_LENGTH, ' ', (EXPECTED), (GOT)); \
   }
 
 #endif

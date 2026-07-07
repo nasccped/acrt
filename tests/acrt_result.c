2f134e4b91a43e21843f42f0b1332d77d664fe78
Nasccped <pedro.main.tec@gmail.com>
2026-07-07 20:29:17 -0300

    fix: due to latest main source changes

--- tests/acrt_result.c
+++ tests/acrt_result.c
@@ -1,13 +1,9 @@
 #include "../src/acrt_result.h"
 #include "test_util.h"
 
-typedef struct __acrt_result result_t;
-
-// Asserts if the 'result.context.line' is equals to the expected.
 #define ASSERT_CONTEXT_LINE(EXPECTED, GOT)                                     \
   ASSERT_EQ_INT("ResultContextLine", (EXPECTED), (GOT))
 
-// Asserts if the 'result.context.name' is equals to the expected.
 #define ASSERT_CONTEXT_NAME(EXPECTED, GOT)                                     \
   ASSERT_EQ_STRING("ResultContextNmae", (EXPECTED), (GOT))
 
@@ -15,30 +11,19 @@ typedef struct __acrt_result result_t;
 // place (on the macro call).
 #define ASSERT_DATA(RESULT) ASSERT("ResultData", (RESULT))
 
-// Asserts if the context kind enum coercion is equals to the expected integer.
 #define ASSERT_KIND(EXPECTED, GOT)                                             \
   ASSERT_EQ_INT("ResultKind", (int)(EXPECTED), (int)(GOT))
 
-// Asserts if the result is passed (success).
 #define ASSERT_PASSED(EXPECTED, GOT)                                           \
   ASSERT_EQ_INT("ResultPassed", (EXPECTED), (GOT))
 
-// Context line related assertions.
 assertion_result_t context_line();
-
-// Context name related assertions.
 assertion_result_t context_name();
-
-// Inner data related assertions.
 assertion_result_t data();
-
-// If the assertion is passed.
 assertion_result_t passed();
-
-// Result kind related assertions.
 assertion_result_t result_kind();
 
-static result_t result;
+static acrt_result_t result;
 
 int main() {
   GENERATE_TEST_CASES(
@@ -79,30 +64,30 @@ assertion_result_t context_name() {
 assertion_result_t data(void) {
   char *string = "Some string";
   result = acrt_result_from_single_pointer("", 2, string);
-  ASSERT_DATA(result.data.single_pointer == string);
+  ASSERT_DATA(result.data.boolean_pointer == string);
 
   return ASSERTION_PASSED;
 }
 
 assertion_result_t passed(void) {
   result = acrt_result_from_int("", 2, 2);
-  ASSERT_PASSED(1, result.passed);
+  ASSERT_PASSED(1, acrt_result_is_passed(&result));
 
   result = acrt_result_from_int("", 2, 0);
-  ASSERT_PASSED(0, result.passed);
+  ASSERT_PASSED(0, acrt_result_is_passed(&result));
 
   result = acrt_result_from_single_pointer("", 2, "temp string");
-  ASSERT_PASSED(1, result.passed);
+  ASSERT_PASSED(1, acrt_result_is_passed(&result));
 
   result = acrt_result_from_single_pointer("", 2, NULL);
-  ASSERT_PASSED(0, result.passed);
+  ASSERT_PASSED(0, acrt_result_is_passed(&result));
 
   return ASSERTION_PASSED;
 }
 
 assertion_result_t result_kind(void) {
   result = acrt_result_from_int("", 1, 1);
-  ASSERT_KIND(SIMPLE_BOOLEAN_ASSERTION_KIND, result.kind);
+  ASSERT_KIND(INTEGER_BOOLEAN_ASSERTION_KIND, result.kind);
 
   result = acrt_result_from_single_pointer("", 1, NULL);
   ASSERT_KIND(POINTER_BOOLEAN_ASSERTION_KIND, result.kind);

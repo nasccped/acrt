e29497d0ce9a1ab24c94c17eb6ebd8893780fcfa
Nasccped <pedro.main.tec@gmail.com>
2026-07-08 17:55:34 -0300

    chore: introduce variables also

--- examples/01-basics.c
+++ examples/01-basics.c
@@ -1,26 +1,30 @@
 #include "../src/acrt.h"
 
+static int universal_true = 1;
+static double pi = 3.14;
+static char *text = "my string";
+static void *integer_ptr = &universal_true;
+
 int main(int argc, char *argv[]) {
   // Macro as default constructor.
   acrt_t acrt = ACRT_NEW();
 
   // Assert any number like value.
-  ACRT_BOOL(&acrt, 1);
-  ACRT_BOOL(&acrt, (int)3.14);
+  ACRT_BOOL(&acrt, universal_true);
+  ACRT_BOOL(&acrt, pi);
+  // also literals
+  ACRT_BOOL(&acrt, 2);
 
   // Assert pointers.
-  int some_integer = 3;
-  int *int_ptr = &some_integer;
-  char *char_ptr = "my string";
-  ACRT_BOOL(&acrt, int_ptr);
-  ACRT_BOOL(&acrt, char_ptr);
+  ACRT_BOOL(&acrt, text);
+  ACRT_BOOL(&acrt, integer_ptr);
 
   // Assert dynamic values and use it as condition.
   char *may_fail = argc > 1 ? argv[1] : NULL;
 
   // BUG: This only succeeds when passing args to exe call.
   if (ACRT_BOOL(&acrt, may_fail)) {
-    printf("may fail holds the '%s' string.\n\n", may_fail);
+    printf("'may fail' variable holds the '%s' string.\n\n", may_fail);
   }
 
   printf("All assertions \e[92mdone\e[0m with no errors...\n");

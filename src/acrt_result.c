472aee2850a45da884b699b8603906c370d98102
Nasccped <pedro.main.tec@gmail.com>
2026-07-17 21:43:54 -0300

    ref/impl: read full commit message
    Several changes was done:
    1. `acrt_result_t` was rebuilt from scratch to fit better 'getters'
       modeling
    2. boolean assertions expects an `intptr_t` now since converting
       negative double falls down to `0` value (it was `uintptr_t`, isn't?)
    3. new getters implemented to help within `acrt_display.c` file. No
       macro manual handling anymore, just delegate it to 'motherfile'...

--- src/acrt_result.c
+++ src/acrt_result.c
@@ -32,37 +32,113 @@
     .context = {.name = (NAME), .line = (LINE) }                               \
   }
 
+char *acrt_result_get_kind_as_str(acrt_result_t *self) {
+  if (!self)
+    return NULL;
+
+  switch (self->kind) {
+  case IGNORED_ASSERTION_KIND:
+    return "ignored";
+
+  case BOOLEAN_ASSERTION_KIND:
+    return "boolean";
+
+  case EQ_ASSERTION_KIND:
+    return "eq";
+  }
+
+  return "undefined";
+}
+
+int acrt_result_is_ignored(acrt_result_t *self) {
+  return self ? self->kind == IGNORED_ASSERTION_KIND : 0;
+}
+
 int acrt_result_is_passed(acrt_result_t *self) {
   if (!self)
     return 0;
 
-  switch (self->status) {
+  switch (self->kind) {
+  case IGNORED_ASSERTION_KIND:
+    return 0;
 
-    // passed conditions
-  case PASSED_ASSERTION_WITHOUT_WARNING:
-  case PASSED_ASSERTION_WITH_WARNING:
-    return 1;
+  case BOOLEAN_ASSERTION_KIND:
+    return self->data.boolean_assertion.kind == POINTER_VALUE_KIND
+               ? (self->data.boolean_assertion.value.pointer ? 1 : 0)
+           : self->data.boolean_assertion.value.integer ? 1
+                                                        : 0;
 
-    // failed conditions
-  case FAILED_ASSERTION:
-  case IGNORED_ASSERTION:
-    break;
+  case EQ_ASSERTION_KIND:
+    return self->data.eq_assertion.result;
+  }
+
+  return 0;
+}
+
+int acrt_result_is_warning(acrt_result_t *self) {
+  if (!self)
+    return 0;
+
+  switch (self->kind) {
+  case IGNORED_ASSERTION_KIND:
+  case BOOLEAN_ASSERTION_KIND:
+    return 0;
+
+  case EQ_ASSERTION_KIND:
+    return self->data.eq_assertion.warning != NO_WARNING;
   }
 
   return 0;
 }
 
 acrt_result_t acrt_result_new_bool(const char *name, const unsigned int line,
-                                   uintptr_t value, int is_ptr) {
-  acrt_result_t result = {.context = {.name = name, .line = line},
-                          .status = value ? PASSED_ASSERTION_WITHOUT_WARNING
-                                          : FAILED_ASSERTION};
+                                   intptr_t value, int is_ptr) {
+  acrt_result_t result = WITH_CTX_ONLY(name, line);
+  result.kind = BOOLEAN_ASSERTION_KIND;
+
   if (is_ptr) {
-    result.kind = POINTER_BOOLEAN_ASSERTION_KIND;
-    result.data.single_pointer = (void *)value;
+    result.data.boolean_assertion.kind = POINTER_VALUE_KIND;
+    result.data.boolean_assertion.value.pointer = (void *)value;
   } else {
-    result.kind = INTEGER_BOOLEAN_ASSERTION_KIND;
-    result.data.integer_cast = (int)value;
+    result.data.boolean_assertion.kind = INTEGER_VALUE_KIND;
+    result.data.boolean_assertion.value.integer = (int)value;
   }
+
+  return result;
+}
+
+acrt_result_t acrt_result_new_eq(const char *name, const unsigned int line,
+                                 void *left, void *right,
+                                 int (*eq_func)(void *, void *)) {
+  acrt_result_t result = WITH_CTX_ONLY(name, line);
+  int temp;
+
+  result.kind = EQ_ASSERTION_KIND;
+  result.data.eq_assertion.params.left = left;
+  result.data.eq_assertion.params.right = right;
+
+  if (!eq_func) {
+    result.data.eq_assertion.warning = EQ_FUNC_IS_NULL_WARNING;
+    result.data.eq_assertion.result = 0;
+    return result;
+  }
+
+  temp = eq_func(left, right);
+
+  // check if pair points to same address
+  if (left == right) {
+    result.data.eq_assertion.warning = SAME_ADDRESS_PARAMS_WARNING;
+
+    // if same address pair returns false (not expected)
+    if (!temp)
+      result.data.eq_assertion.warning = SAME_ADDRESS_PARAMS_ARE_DIFF_WARNING;
+
+    result.data.eq_assertion.result = temp;
+
+  } else {
+    result.data.eq_assertion.warning = NO_WARNING;
+    result.data.eq_assertion.result = temp;
+  }
+
   return result;
 }
